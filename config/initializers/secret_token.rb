cb3ef94f2353b74f39324733a695d5bb007338b1
Anisul <anisul.huq90@gmail.com>
2013-09-08 00:44:12 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+York::Application.config.secret_key_base = '7f99d055415e579d4563da4d7a5691fbce95305041bc69e4ab6c417d0874b8f3205a9ad4c6eaaf4729b8520741e431a0076437c5d714abda892ff21797f3ef17'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
